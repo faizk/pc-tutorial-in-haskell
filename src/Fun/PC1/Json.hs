8b1544393114c8c56efc908b2cba92a0da878cbc
Faiz Kazi <faiz@titan.ocn.ne.jp>
2024-08-16 19:00:27 +0900

    + surr2, arrP

--- src/Fun/PC1/Json.hs
+++ src/Fun/PC1/Json.hs
@@ -11,6 +11,7 @@ import Fun.PC1
     , foll
     , orElse
     , zeroOrMore
+    , delimP
     )
 
 import qualified Fun.Json as J
@@ -21,6 +22,9 @@ numP = pmap J.Num $ pmap toInteger naturalP
 surr :: Parser a -> Parser b -> Parser b
 surr pa pb = pmap (snd . fst) $ pa `foll` pb `foll` pa
 
+surr2 :: Parser a -> Parser b -> Parser c -> Parser b
+surr2 pa pb pc = pmap (snd . fst) $ pa `foll` pb `foll` pc
+
 stringP :: Parser J.Json
 stringP = pmap J.Str $ surr q $ zeroOrMore (charP (== '"'))
   where q = charP (=='"')
@@ -30,5 +34,13 @@ boolP = p True "true" `orElse` p False "false"
   where
     p b s = pmap (const $ J.Bool b) $ strP s
 
+arrP :: Parser J.Json
+arrP = pmap J.Arr $ surr2 (c '[') lp (c ']')
+  where
+    c x = charP (== x)
+    lp = delimP commaP parse
+    commaP = c ','
+
 parse :: Parser J.Json
 parse = numP `orElse` stringP `orElse` boolP
+  `orElse` arrP
