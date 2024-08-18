80f0e76a5e81638f973382dfa0a2cb6e037af4cf
Faiz Kazi <faiz@titan.ocn.ne.jp>
2024-08-18 18:40:04 +0900

    + Arbitrary Sxpr

--- src/Fun/Sxpr.hs
+++ src/Fun/Sxpr.hs
@@ -3,6 +3,8 @@ module Fun.Sxpr
     , Val(..)
     ) where
 
+import Test.QuickCheck
+
 data Val
   = Num Int
   | Str String
@@ -19,3 +21,32 @@ data Sxpr
   | Uqt Sxpr
   deriving (Eq, Show)
 
+instance Arbitrary Val where
+  arbitrary = oneof [ Num <$> arbitrary
+                    , return T, return F
+                    ]
+
+instance Arbitrary Sxpr where
+  arbitrary = sized arbitrary'
+    where
+      arbitrary' 0 = return Nil
+      arbitrary' n =
+        oneof [ Sym <$> genSmallStr 1 8
+              , Lit <$> arbitrary
+              , Qt <$> resize (n - 1) arbitrary
+              , Qqt <$> resize (n - 1) arbitrary
+              , Uqt <$> arbitrary
+              , genPair (n `div` 2)
+              , genList (n `div` 4)
+              ]
+      genPair n = do
+        l <- resize 1 arbitrary
+        r <- resize ((n - 1) `max` 0) arbitrary
+        return $ Pair l r
+      genList n = do
+        v <- vectorOf n (arbitrary :: Gen Sxpr)
+        return $ foldl Pair Nil v
+      genSmallStr u v = do
+        l <- choose (u, v)
+        vectorOf l genAlphaNumChar
+      genAlphaNumChar = elements ['a'..'z']
