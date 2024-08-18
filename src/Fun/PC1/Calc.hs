69ce3d81fd0f82031be1f4e8ee2330b3e5a13345
Faiz Kazi <faiz@titan.ocn.ne.jp>
2024-08-18 15:24:45 +0900

    improved Calc

--- src/Fun/PC1/Calc.hs
+++ src/Fun/PC1/Calc.hs
@@ -13,18 +13,20 @@ data Expr
   | Div Expr Expr
   deriving Show
 
-op :: Parser Int -> Char -> Parser Int -> (Int -> Int -> Int) -> Parser Int
-op lp c rp f = pmap f' $ lp `foll` ws (char c) `foll` rp
-  where f' ((a, _), b) = f a b
+type Op = Int -> Int -> Int
+
+op2 :: Parser Int -> Parser Op -> Parser Int -> Parser Int
+op2 lp opp rp = pmap f' $ lp `foll` ws opp `foll` rp
+  where f' ((a, f), b) = f a b
 
 calcP :: Parser Int
 calcP = subP
   where
-    subP = op addP '-' subP (-) `orElse` addP
-    addP = op multP '+' addP (+) `orElse` multP
-    multP = op divP '*' multP (*) `orElse` divP
-    divP = op ep '/' divP div `orElse` ep
-    ep = ws (parens subP `orElse` constP)
+    subP   = op2 addP  (opp '-' (-)) subP  `orElse` addP
+    addP   = op2 multP (opp '+' (+)) addP  `orElse` multP
+    multP  = op2 divP  (opp '*' (*)) multP `orElse` divP
+    divP   = op2 bp    (opp '/' div) divP  `orElse` bp
+    bp     = ws (parens subP `orElse` constP)
     constP = pmap fromIntegral integerP
+    opp c f = const f `pmap` ws (char c)
     parens p = surr2 (char '(') p (char ')')
-    
