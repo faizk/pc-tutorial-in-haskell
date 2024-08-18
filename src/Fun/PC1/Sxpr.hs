eee9b0d240e6fdc7e91f266dcd349332d374a1c9
Faiz Kazi <faiz@titan.ocn.ne.jp>
2024-08-18 17:40:05 +0900

    + Fun/PC1/Sxpr

---
+++ src/Fun/PC1/Sxpr.hs
@@ -0,0 +1,65 @@
+module Fun.PC1.Sxpr
+    ( sxprP
+    ) where
+
+import qualified Fun.Sxpr as S
+
+import Fun.PC1
+import Data.Char (isSpace)
+
+zilchP :: Parser S.Sxpr
+zilchP = pmap (const S.Nil) $ ws (char '(') `foll` ws (char ')')
+
+symCP :: Parser Char
+symCP = charP f
+  where
+    f c = case c of
+      ',' -> False
+      '`' -> False
+      '\\' -> False
+      '(' -> False
+      ')' -> False
+      _ | isSpace c -> False
+      _   -> True
+
+parensP :: Parser a -> Parser a
+parensP p = pmap f $ char '(' `foll` ws p `foll` char ')'
+  where f ((_, a), _) = a
+
+symP :: Parser S.Sxpr
+symP = S.Sym `pmap` oneOrMore symCP
+
+cellP' :: Parser S.Sxpr
+cellP' = pmap f $ ws sxprP `foll` dot `foll` ws sxprP
+  where
+    f ((l, _), r) = S.Pair l r
+    dot = ws $ char '.'
+
+listP' :: Parser S.Sxpr
+listP' = asPair `pmap` delimP wsP sxprP
+  where
+    asPair (s:ss) = S.Pair s (asPair ss)
+    asPair [] = S.Nil
+
+listP :: Parser S.Sxpr
+listP = parensP listP'
+
+cellP :: Parser S.Sxpr
+cellP = parensP cellP'
+
+atomP :: Parser S.Sxpr
+atomP = boolP `orElse` intP `orElse` symP `orElse` zilchP
+  where
+    boolP = b "#t" S.T `orElse` b "#f" S.F
+    b s f = (S.Lit . const f) `pmap` strP s
+    intP = (S.Lit . (S.Num . fromIntegral)) `pmap` integerP
+
+sxprP :: Parser S.Sxpr
+sxprP = ws $ qSxprP `orElse` atomP `orElse` listP `orElse` cellP
+  where
+    qSxprP = qteP `orElse` qqteP `orElse` uqteP
+    qteP = q '\'' S.Qt
+    qqteP = q '`' S.Qqt
+    uqteP = q ',' S.Uqt
+    q :: Char -> (S.Sxpr -> S.Sxpr) -> Parser S.Sxpr
+    q c f = (f . snd) `pmap` (char c `foll` sxprP)
